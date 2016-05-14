a97a6cf266ca2d601c13a79e3ec92b9b20dc25b5
Josh Marshall <jrmarsha@mtu.edu>
2016-05-14 16:11:16 -0400

    Work on moving to loading the file information directly into a well formed network structure suitable for parallelization of processing later on in developement.  Currently does not compile due to incorrect use of unordered_map's, but this will be fixed when I can access documentation.  The internal changes of this commit are actually kind of neat!

--- Makefile
+++ Makefile
@@ -1,14 +1,29 @@
 CPP=g++
-CFLAGS=-g -O0
-LIBS=
+CFLAGS=-g -O0 -pipe -Wall -std=c++0x
+##LIBS=
 
 EXEC=TF-cluster
 
-SOURCES=main.cpp
-OBJECTS=main.o
+SOURCES=main.cpp auxillaryUtilities.cpp nodeNetwork.cpp gEdge.cpp gNode.cpp
+OBJECTS=main.o auxillaryUtilities.o nodeNetwork.o gEdge.o gNode.o
 
 all:$(OBJECTS)
 	$(CPP) $(CFLAGS) $(LIBS) $(OBJECTS) -o $(EXEC)
 
-$(OBJECTS):$(SOURCES)
-	$(CPP) $(CFLAGS) -c $<
+main.o:main.cpp pairTable.hpp auxillaryUtilities.hpp
+	$(CPP) $(CFLAGS) -c main.cpp
+
+auxillaryUtilities.o:auxillaryUtilities.cpp auxillaryUtilities.hpp
+
+nodeNetwork.o:nodeNetwork.cpp nodeNetwork.hpp auxillaryUtilities.hpp gEdge.hpp gNode.hpp
+	$(CPP) $(CFLAGS) -c nodeNetwork.cpp
+
+gEdge.o:gEdge.cpp gEdge.hpp gNode.hpp
+	$(CPP) $(CFLAGS) -c gEdge.cpp
+
+gNode.o:gNode.cpp gEdge.hpp gNode.hpp nodeNetwork.hpp
+	$(CPP) $(CFLAGS) -c gNode.cpp
+
+clean:
+	rm -f $(OBJECTS)
+	rm -f $(EXEC)
--- main.cpp
+++ main.cpp
@@ -1,139 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <errno.h>
-#include <cblas.h>
-#include <vector>
-#include <queue>
-#include <utility>
-#include <string.h>
-
-using namespace std;
 /***********************************************************************
 This is a C++ reimplementation of TF-Cluster.  The goal of this project 
 to improve the time and space requirements over the perl implementation.
 
 Input: 1 file name or input from stdin.
 ***********************************************************************/
+#include <stdio.h>
+
+#include "auxillaryUtilities.hpp"
+#include "nodeNetwork.hpp"
 
 
 
-vector<string> tokenizeString(char *input, 
-                                      const char *delimitors = " \t,"){
-  vector<string> tr;
-  char *tokenStart;
+//nodeNetwork tripleLinkNetwork(pairTable toParse){
   
-  
-  tokenStart = strtok(input, delimitors);
-  
-  while(tokenStart){
-    tr.push_back(string(tokenStart));
-    tokenStart = strtok(NULL, delimitors);
-  }
-  
-  return tr;
-}
+//}
 
-
-class pairTable{
-  public:
-  vector<vector<pair<string, double>>> data;
-  vector<string> columnLabels;
+//vector<vector<string>> tripleLinkResult(nodeNetwork toParse){
   
-  void loadFromFile(FILE *input){
-    char *line = NULL;
-    vector<string> tokens;
-
-    getline(&line, NULL, stdin);
-    tokens = tokenizeString(line);
-    
-    while(feof(stdin)){
-      free(line); line = NULL;
-      for(size_t i = 0; i < tokens.size(); i+=2){
-        data[i].push_back(pair<string, double>(tokens[i], atof(tokens[i+1].c_str())));
-      }
-    }
-    
-    free(line); line = NULL;
-    
-    sortColumns();
-  }
-  
-  
-  void sortColumns(){
-    for(size_t i = 0; i < data.size(); i++) data[i] = sortColumn(data[i]);
-  }
-  
-  //quickmerge, high to low
-  vector<pair<string, double>> sortColumn(vector<pair<string, double>> toSort){
-    if(toSort.size() < 2) return toSort;
-    size_t i = 0, j;
-    while(toSort[i].second < toSort[i+1].second);
-    
-    vector<pair<string, double>> left, right, merged;
-    
-    //TODO
-    for(j = 0; j < i; j++) left.push_back(toSort[i]);
-    for(;j < toSort.size(); j++) right.push_back(toSort[i]);
-    
-    left = sortColumn(left);
-    right = sortColumn(right);
-    
-    i = j = 0;
-    while(i < left.size() && j < right.size()){
-      if(left[i] > right[j]){
-        merged.push_back(left[i++]);
-      }else{
-        merged.push_back(right[j++]);
-      }
-    }
-    
-    while(i < left.size()) merged.push_back(left[i++]);
-    while(j < right.size()) merged.push_back(right[j++]);
-    
-    return merged;
-  }
-  
-  
-};
-
-
-pairTable prune(const pairTable toPrune, unsigned char keepTopN = 100){
-  pairTable pruned;
-  
-  for(size_t i = 0; i < toPrune.columnLabels.size(); i++){
-    pruned.columnLabels.push_back(toPrune.columnLabels[i]);
-    pruned.data.push_back(vector<pair<string, double>>());
-    for(unsigned char j = 0; j < keepTopN; j++){
-      pruned.data[i].push_back(toPrune.data[i][j]);
-    }
-  }
-  
-  return pruned;
-}
-
-
-int verifyInput(int argc, char **argv){
-  
-  if( argc > 2 ) return EINVAL;
-  
-  if(argc == 2) freopen(argv[1], "r", stdin);
-  
-  return 0;
-}
+//}
 
 
 int main(int argc, char **argv){
-  pairTable inputTable;
   
   int error = verifyInput(argc, argv);
   if(error) return error;
   
-  pairTable corrData;
-  
+  nodeNetwork corrData;
   corrData.loadFromFile(stdin);
+  fclose(stdin);
   
-  corrData = prune(corrData);
+  corrData.prune();
+  //corrData.prepareTripleLink();
+  //corrData.TripleLink();
+  
   
   return 0;
 }
