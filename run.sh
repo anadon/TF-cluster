14e6da4b3776256953e0b9b242558e7f5c595c3c
anadon <joshua.r.marshall.1991@gmail.com>
2017-01-05 15:07:59 -0500

    Adjust usage for organization scheme used for examples.

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-/usr/bin/time -v ./tf-cluster -1 1.5 -2 1.2 -3 0.8 -c spearman -e correlation-matrix/example/rma_salt-cleaned.txt -t correlation-matrix/example/genelist-cleaned.txt -k 100
-
-#valgrind -v --leak-check=full ./tf-cluster -1 1.5 -2 1.2 -3 0.8 -c "spearman" -e At_stem_rma_july2012_AGI129_final.txt -t AT_TF_list.csv -k 100
+/usr/bin/time -v ./tf-cluster -1 1.5 -2 1.2 -3 0.8 -c spearman -e correlation-matrix/example/base-clean/rma_salt.txt -t correlation-matrix/example/base-clean/TFs.txt -k 100
